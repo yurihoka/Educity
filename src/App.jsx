3ebee4be9666e53b623579b7a2e0e57d1c7f2e54
yurikahokama <hokama@engineerforce.io>
2025-10-07 22:00:39 +0900

    create campus component

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Hero from "./Components/Hero/Hero";
 import Programs from "./Components/Program/Programs";
 import Title from "./Components/Title/Title";
 import About from "./Components/About/About";
+import Campus from "./Components/Campus/Campus";
 
 const App = () => {
   return (
@@ -14,6 +15,8 @@ const App = () => {
         <Title subTitle="Our Program" title="What We Offer" />
         <Programs />
         <About />
+        <Title subTitle="Gallery" title="Campus Photos" />
+        <Campus />
       </div>
     </div>
   );
