c1fc471393d52f128556147d5c9116ed37c1b7b3
yurikahokama <hokama@engineerforce.io>
2025-10-07 21:47:41 +0900

    update navbar style

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,21 +1,31 @@
-import React from 'react'
-import './Navbar.css'
-import logo from '../../assets/logo.png'
+import React, { useEffect, useState } from "react";
+import "./Navbar.css";
+import logo from "../../assets/logo.png";
 
 function Navbar() {
+  const [sticky, setSticky] = useState(false);
+
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      window.scrollY > 50 ? setSticky(true) : setSticky(false);
+    });
+  }, []);
+
   return (
-    <nav className='container'>
-      <img src={logo} alt="" className='logo'/>
+    <nav className={`container ${sticky ? "dark-nav" : ""}`}>
+      <img src={logo} alt="" className="logo" />
       <ul>
         <li>Home</li>
         <li>Program</li>
         <li>About us</li>
         <li>Campus</li>
         <li>Testimonials</li>
-        <li><button className='btn'>Contact us</button></li>
+        <li>
+          <button className="btn">Contact us</button>
+        </li>
       </ul>
     </nav>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
