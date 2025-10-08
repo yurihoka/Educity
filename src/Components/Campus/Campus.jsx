c5657b221c01fbc5325dfd7ec65716177967f41b
yurikahokama <hokama@engineerforce.io>
2025-10-08 16:35:19 +0900

    fix the folder name

--- src/Components/Campus/Campus.jsx
+++ src/Components/Campus/Campus.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import "./Campus.css";
-import gallery_1 from "../../Assets/gallery-1.png";
-import gallery_2 from "../../Assets/gallery-2.png";
-import gallery_3 from "../../Assets/gallery-3.png";
-import gallery_4 from "../../Assets/gallery-4.png";
-import white_arrow from "../../Assets/white-arrow.png";
+import gallery_1 from "../../assets/gallery-1.png";
+import gallery_2 from "../../assets/gallery-2.png";
+import gallery_3 from "../../assets/gallery-3.png";
+import gallery_4 from "../../assets/gallery-4.png";
+import white_arrow from "../../assets/white-arrow.png";
 
 const Campus = () => {
   return (
